b685f625cc0ae9e46e7837a7cd00af5653bf0206
Mickael Brunet <mickael.brunet64@gmail.com>
2021-08-31 21:24:13 +0200

    Mise en place de la navigation avec les modules BrowserRouter, Switch et Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 //import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; //importation des librairies
+import NotFound from './pages/NotFound';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <h1>Hello React</h1>
-        <Home />
-      </header>
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>*/}
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
