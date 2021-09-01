1b09b86d5f27161de3c6b510a53a949905beb08d
Mickael Brunet <mickael.brunet64@gmail.com>
2021-09-01 14:07:08 +0200

    Mise en place d'un tri pour afficher la population des pays du plus grand au plus petit et ajout du style pour le composant Countries qui englobe toutes les cartes.

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([]);
+    const [sortedData, setSortedData] = useState([]);
+    const [playOnce, setPlayOnce] = useState(true);
 
     useEffect(() => {
+        if (playOnce) {
         axios
         .get(
             "http://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
         )
-        .then((res) => setData(res.data));
+        .then((res) => 
+            setData(res.data));
+            setPlayOnce(false);
+        }
 
-    }, []);
+        const sortedCountry = () => {
+            const countryObject = Object.keys(data).map((i) => data[i]);
+            const sortedArray = countryObject.sort((a,b) => {
+                return b.population - a.population
+            });
+            sortedArray.length = 30;
+            setSortedData(sortedArray);
+        };
+        sortedCountry();
+
+    }, [data, playOnce]);
 
     return (
     <div className="countries">
         <ul className="countries-list">
-            {data.map((country) => (
-                <li>{country.name}</li>
+            {sortedData.map((country) => (
+                <Card country={country} key={country.name}/>
             ))}
         </ul>
     </div>
